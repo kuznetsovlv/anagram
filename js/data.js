7a738e4cd1ce74fac79756a7d2ad004702e4c5db
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-03-01 01:21:12 +0300

    word list added

---
+++ js/data.js
@@ -0,0 +1,69 @@
+(function () {
+	"use strict";
+
+	var words = [
+		'осень'
+	];
+
+	function Data (arr) {
+		if (arr instanceof Array)
+			this.addWords(arr);
+		else if (typeof arr = 'string')
+			this.addWord(arr);
+		else
+			throw "Incorrect initial data";
+	}
+
+	Object.defineProperties(Data.prototype, {
+		addWord: {
+			value: function (w) {
+				if (!w || typeof w !== 'string' || /[^a-zA-Zёа-яЁА-Я].test(w)/)
+					throw ['Incorrect word:', w].join(' ');
+				var length = w.length;
+				if (!this[length])
+					this[length] = [w];
+				else
+					this[length].push(w);
+				return this;
+			},
+			writable: false,
+			enumerable: false,
+			configurable: false
+		},
+		addWords: {
+			value: function (arr) {
+				if (!(arr instanceof Array))
+					throw "Incorrect array";
+				for (var i = 0, l = arr.length; i < l; ++i)
+					this.addWord(arr[i]);
+				return this;
+			},
+			writable: false,
+			enumerable: false,
+			configurable: false
+		},
+		getWord: {
+			value: function (l) {
+				return this[l] ? this[l][Math.floor(Math.random() * this[l].length)] : undefined;
+			},
+			writable: false,
+			enumerable: false,
+			configurable: false
+		},
+		lengthes: {
+			get: function () {
+				var res = [];
+				for (var key in this) {
+					key = parseInt(key);
+					if (key)
+						res.push(key);
+				}
+				return res;
+			},
+			enumerable: false,
+			configurable: false
+		}
+	});
+
+	window.wordList = new Data(words);
+})()
